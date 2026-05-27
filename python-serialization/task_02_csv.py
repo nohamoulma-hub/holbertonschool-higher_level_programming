9f0ab1d367e6d360320b3b14f9cb61447b2c652e
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-27 14:35:36 +0200

    fix: correct file handling in convert_csv_to_json function

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -9,9 +9,9 @@ def convert_csv_to_json(csv_filename):
         with open(csv_filename, "r", encoding="utf-8") as csv_file:
             csv_reader = csv.DictReader(csv_file)
             data = list(csv_reader)
-        
-        with open("data.json", "w", encoding="utf-8") as json_file:
-            json.dump(data, json_file)
+
+        with open(csv_filename, "w", encoding="utf-8") as data.json:
+            json.dump(data, data.json)
 
     except FileNotFoundError:
         return False
