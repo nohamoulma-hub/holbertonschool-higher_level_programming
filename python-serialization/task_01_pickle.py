8ea163125341d0d57164f7d622c27372b432c5a8
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-27 13:37:42 +0200

    refactor error handling in serialization methods

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -16,12 +16,17 @@ class CustomObject():
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
 
-        with open(filename, "wb") as f:
-            pickle.dump(self, f)
+        except Exception:
+            return None
 
     @classmethod
     def deserialize(cls, filename):
-
-        with open(filename, "rb") as f:
-            return pickle.load(f)
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except Exception:
+            return None
