0c5dc7c576eae8fe99e6210f8ce329b61a97b4a2
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-08 13:17:14 +0200

    feat: add script to retrieve and display all states from the database

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module getting all states"""
+
+
+import sys
+import MySQLdb
+
+if __name__=="__main__":
+
+    mysql_username = sys.argv[1] # root
+    mysql_password = sys.argv[2] # password
+    database_name = sys.argv[3] # name database
+
+    db = MySQLdb.connect(
+        host="localhost", 
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name)
+    cur = db.cursor() # je créer le stylo
+    cur.execute(
+        "SELECT * FROM states ORDER BY id ASC"
+        ) # j'écris ma requête avec le stylo
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
