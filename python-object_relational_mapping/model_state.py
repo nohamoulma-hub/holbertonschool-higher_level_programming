6bb7c7bf3c366bf588d92a1cc86bdea921c1444d
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-08 18:42:07 +0200

    fix: add autoincrement to state model id column

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,5 +11,5 @@ Base = declarative_base()
 class State(Base):
 
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
