90f0bc9ca2d08c212e6704d16205ea6a0a71bf84
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-08 13:42:08 +0200

    feat: add script to filter states starting with 'N' from the database

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module getting all states and filter that"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]  # root
+    mysql_password = sys.argv[2]  # password
+    database_name = sys.argv[3]  # name database
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name)
+    cur = db.cursor()  # je créer le stylo
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        )  # j'écris ma requête avec le stylo
+    rows = cur.fetchall()  # récupère les résultats stockés
+    for row in rows:  # Lit chaque résultat un à un 
+        print(row)  # affiche les tupples
+    cur.close()
+    db.close()
