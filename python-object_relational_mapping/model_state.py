f88ec42beaf8c148920cd7fa7655389b06a0504d
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-08 18:39:48 +0200

    fix: improve formatting of state model attributes

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """ First state model """
 
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
-    __tablename__= 'states'
+
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
