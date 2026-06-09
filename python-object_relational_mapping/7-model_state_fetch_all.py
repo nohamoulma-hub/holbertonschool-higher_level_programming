2164f6cef6150cda36f2376ad4998ffef72919c4
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-09 11:10:19 +0200

    feat: add script to list all states using SQLAlchemy

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module who list all states via SQLAlchemy"""
+
+
+import sys
+from model_state import Base, State
+# importation du model de class à utiliser
+from sqlalchemy import create_engine
+# créer la connection à la base de donnée
+from sqlalchemy.orm import sessionmaker
+# permet de créer des sessions pour interroger la base
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine
+    (
+        f'mysql+mysqldb://{mysql_username}:'
+        f'{mysql_password}@localhost:3306/{database_name}'
+        )
+
+    Session = sessionmaker(bind=engine)  # on retourne une classe
+    session = Session()  # on retourne la vraie session (une instance)
+
+    result_query = session.query(State).order_by(State.id).all()
+    # on stock le resultat de la requête dans une variable
+    for state in result_query:
+        print(f'{state.id}: {state.name}')
+    # on affiche le résultat
