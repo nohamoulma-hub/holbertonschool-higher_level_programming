99ca3800efb1f0c32f5f246360ccda7f16689b9e
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-10 22:26:09 +0200

    feat: implement a simple HTTP server API using http.server module

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Module who Develop a simple API using
+Python with the `http.server` module"""
+
+
+import http.server
+import json
+
+
+class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    """class set up a simple HTTP server"""
+
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)  # envoie le code de statut
+            self.end_headers()  # termine les headers
+            self.wfile.write(
+                "Hello, this is a simple API!".encode())  # envoie le contenu
+
+        elif self.path == "/data":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(
+                json.dumps
+                ({"name": "John", "age": 30, "city": "New York"})
+                .encode()
+                )
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write("OK".encode())
+
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write("Endpoint not found".encode())
+
+
+if __name__ == "__main__":
+
+    server = http.server.HTTPServer(("", 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
