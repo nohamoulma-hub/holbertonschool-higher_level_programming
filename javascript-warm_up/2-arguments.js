f54d8906cd3451d904ef3766bdcdb042a22797b8
nohamoulma-hub <noham.oulma@gmail.com>
2026-07-01 17:59:12 +0200

    fix: correct formatting and improve readability in 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 
-const argv = process.argv
+const argv = process.argv;
 if (argv.length === 2) {
-    console.log('No argument');
-}
-
-if(argv.length === 3) {
-    console.log('Argument found');
-}
-
-if(argv.length === 4) {
-    console.log('Arguments found');
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
